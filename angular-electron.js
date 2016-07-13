757d17c714292d9c5ad668632cec48fa31f5e6ef
André Wehlert <realsancezz@gmail.com>
2016-07-13 10:16:09 +0200

    Updating compiled sources.

--- angular-electron.js
+++ angular-electron.js
@@ -19,7 +19,7 @@ angular.module('angular-electron').constant('process', electronProcess);
 
 var remoteModules = ['app', 'autoUpdater', 'BrowserWindow', 'contentTracing', 'dialog',
                      'globalShortcut', 'Menu', 'MenuItem', 'powerMonitor', 'powerSaveBlocker',
-                     'protocol', 'webContents', 'tray'];
+                     'protocol', 'webContents', 'Tray'];
 var nodeModules = ['buffer', 'child_process', 'cluster', 'crypto', 'dns', 'events', 'fs', 'http',
                    'https', 'net', 'os', 'path', 'punycode', 'querystring', 'readline', 'stream',
                    'string_decoder', 'tls', 'dgram', 'url', 'util', 'v8', 'vm', 'zlib'];
@@ -27,8 +27,9 @@ var nodeModules = ['buffer', 'child_process', 'cluster', 'crypto', 'dns', 'event
 angular.module('angular-electron').provider('remote', ['$provide', function($provide) {
   var remote = electronRequire('electron').remote;
 
-  function registerElectronModule(_module) {
-    $provide.service(_module, function() {
+  function registerElectronModule(_module, _require) {
+    _require = _require || _module;
+    $provide.service(_require, function() {
       return remote[_module];
     });
   }
@@ -42,6 +43,7 @@ angular.module('angular-electron').provider('remote', ['$provide', function($pro
   }
 
   this.register = registerNodeModule;
+  this.registerFromElectron = registerElectronModule;
 
   this.$get = [function() {
     return remote;
