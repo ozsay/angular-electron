4055d76eb87dc6c08672e6cd51e3949fc696d137
Giorgio Modoni <modogio@gmail.com>
2017-01-23 12:56:49 +0100

    to close issue #9 - add module without electron
    create electronRequire empty object to start module without electron

--- src/process.js
+++ src/process.js
@@ -1,1 +1,16 @@
 angular.module('angular-electron').constant('process', electronProcess);
+
+if (typeof electronRequire == 'undefined') {
+    var electronRequire = function () {
+      return {
+        remote: {
+          getCurrentWindow: function () {
+            return false;
+          },
+          getCurrentWebContents: function () {
+            return false;
+          }
+        },
+      };
+    }
+  }
